d3857e753ca7fdc01af6be52bfb798b3052a083f
Avinash Roy <108816279+billy3b@users.noreply.github.com>
2023-12-08 14:51:23 +0530

    fixed deployment errors

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     typescript:{
-//         ignoreBuildErrors:true
-//     },
-//     eslint:{
-//         ignoreBuildErrors:true,
-//         ignoreDuringBuilds:true,
-//     }
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+      },
+      experimental: {
+        serverActions: true,
+      },
+}
 
 module.exports = nextConfig
